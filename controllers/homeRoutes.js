46fb9e16c4c2684bba5cae25d1f402fbc00140eb
jdutton8 <jennydutton7@gmail.com>
2023-08-17 15:02:36 -0500

    adding realtional data to models

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,51 @@
 const router = require('express').Router();
-const { Posts, User } = require('../models');
+const { Posts, User, Comments } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Posts.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => Posts.get({plain:true}));
+
+    res.render('home', {
+      posts, 
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+      const userData = await User.findByPk(req.session.user_id, {
+          attributes: { exclude: ['password'] },
+          include: [{ model: Posts }],
+      });
+      console.log(userData);
+      res.render('profile', {
+          userData,
+          logged_in: true,
+      });
+  } catch (err) {
+      res.status(500).json(err);
+  }
+}
+);
 
 router.get('/login', (req, res) => {
   try {
     if(req.session.loggedIn) {
-      res.redirect('/profile');
+      res.redirect('/');
       return;
     }
     res.render('login');
@@ -15,7 +55,15 @@ router.get('/login', (req, res) => {
   }
     });
 
-
+ router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+  
+      res.render('signup');
+  }
+  );
 
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -2,6 +2,24 @@ const User = require('./User');
 const Posts = require('./Posts');
 const Comments = require('./Comments');
 
-
+User.hasMany(Posts, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+Posts.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+User.hasMany(Comments, {
+  foreignKey: 'user_id',
+});
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+Posts.hasMany(Comments, {
+  foreignKey: 'post_id'
+});
+Comments.belongsTo(Posts, {
+  foreignKey: 'user_id'
+});
 
 module.exports = { User, Posts, Comments };
